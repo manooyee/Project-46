47654c89e17c1e55f9b3a809eb1e544cd4c715ca
manooyee <72470077+manooyee@users.noreply.github.com>
2021-02-14 13:55:16 -0500

    Add files via upload

---
+++ Money.js
@@ -0,0 +1,25 @@
+class Money{
+    constructor(x,y){
+      var options = {
+          isStatic:false
+      }
+
+      this.body =  this.body = Bodies.rectangle(x, y, 20,20, options);
+      this.image = loadImage("images/money.png");
+      this.visiblity = 255;
+      World.add(world,this.body);
+    
+    }
+
+    display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, 20,20);
+        pop();
+    }
+
+    invisible(){
+        this.visiblity = 0;
+    }
+}
